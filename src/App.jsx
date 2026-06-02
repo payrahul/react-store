e470a9ea8d47d96fd420f3bae3a417c474c82cbf
Rahul <rahulpay006@gmail.com>
2026-06-02 17:31:48 +0530

    checking tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
